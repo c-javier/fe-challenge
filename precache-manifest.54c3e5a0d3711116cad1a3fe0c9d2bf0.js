cca148a257e11191cb8e69ae3b1ec1a340109cdc
c-javier <superqueerhere@gmail.com>
2019-01-30 19:47:15 -0800

    Updates

---
+++ precache-manifest.54c3e5a0d3711116cad1a3fe0c9d2bf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "774b31331349f6f9de0a",
+    "url": "/fe-challenge/static/js/runtime~main.774b3133.js"
+  },
+  {
+    "revision": "d05262b618f52e075d3c",
+    "url": "/fe-challenge/static/js/main.d05262b6.chunk.js"
+  },
+  {
+    "revision": "e91ff07125aecafbbb7b",
+    "url": "/fe-challenge/static/js/1.e91ff071.chunk.js"
+  },
+  {
+    "revision": "d05262b618f52e075d3c",
+    "url": "/fe-challenge/static/css/main.fbac2bd3.chunk.css"
+  },
+  {
+    "revision": "1abc5b9f7811326887740a37301d87a8",
+    "url": "/fe-challenge/index.html"
+  }
+];
--- src/reducers/root/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import categories from '../categories';
-import pros from '../pros';
-
-export default combineReducers({
-  service_roles: categories,
-  list_of_pros: pros,
-});
